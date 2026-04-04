dd5de853943c62152ff8e73d00f22679f8bafdda
zhengmingwu <zhengmingwu@kuaishou.com>
2026-04-04 12:37:56 +0800

    Update

--- run_standard_up.sh
+++ run_standard_up.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
+# for h200 training
+cd /home/dingruiyi/eff_grpo
 export WANDB_MODE=disabled
-
-mkdir -p data/outputs/standard
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export PATH=/usr/local/cuda-12.8/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-12.8/lib64:$LD_LIBRARY_PATH
+export RANK=0
+export WORLD_SIZE=8
+mkdir -p /dry-data/grpo_standard_up
 
 torchrun --nproc_per_node=8 --master_port 19003 \
     fastvideo/train_grpo_qwenimage.py \
@@ -10,21 +15,21 @@ torchrun --nproc_per_node=8 --master_port 19003 \
     --pretrained_model_name_or_path data/qwenimage \
     --vae_model_path data/qwenimage \
     --cache_dir data/.cache \
-    --data_json_path data/qwenimage/rl_embeddings/videos2caption.json \
+    --data_json_path data/qwenimage/rl_embeddings_prompts/videos2caption.json \
     --gradient_checkpointing \
-    --train_batch_size 1 \
+    --train_batch_size 8 \
     --num_latent_t 1 \
     --sp_size 1 \
     --train_sp_batch_size 2 \
     --dataloader_num_workers 4 \
     --gradient_accumulation_steps 12 \
-    --max_train_steps 300 \
+    --max_train_steps 2000 \
     --learning_rate 1e-5 \
     --mixed_precision bf16 \
-    --checkpointing_steps 60 \
+    --checkpointing_steps 25 \
     --allow_tf32 \
     --cfg 0.0 \
-    --output_dir data/outputs/grpo_auto \
+    --output_dir /dry-data/grpo_standard_up \
     --h 720 \
     --w 720 \
     --t 1 \
@@ -41,6 +46,6 @@ torchrun --nproc_per_node=8 --master_port 19003 \
     --ignore_last \
     --timestep_fraction 0.67 \
     --init_same_noise \
-    --clip_range 1e-4 \
+    --clip_range 0.1 \
     --adv_clip_max 5.0 \
-    --selective_checkpointing 0
+    --selective_checkpointing 0.0
